930862e38bd8909e810c86c4f8286dd2b0054243
Dauren Absattarov <dauren.absattarov@gmail.com>
2023-05-30 13:58:15 +0600

    Commit guide project

---
+++ src/app/components/create-product/create-product.component.ts
@@ -0,0 +1,48 @@
+import { Component,OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms'
+import { ModalService } from 'src/app/services/modal.service';
+import { ProductsService } from 'src/app/services/products.service';
+
+@Component({
+  selector: 'app-create-product',
+  templateUrl: './create-product.component.html',
+  styleUrls: ['./create-product.component.scss']
+})
+export class CreateProductComponent implements OnInit{
+
+  constructor(private productService: ProductsService,
+    private modelService: ModalService){
+
+  }
+
+  form = new FormGroup({
+    title: new FormControl<string>('',[
+      Validators.minLength(6)
+    ])
+  })
+
+  submit(){
+    this.productService.create({
+      title: this.form.value.title as string,
+      price: 13.5,
+      description: 'lorem ipsum set',
+      image: 'https://i.pravatar.cc',
+      category: 'electronic',
+      rating:{
+        rate:42,
+        count:1
+      }
+  }).subscribe(()=>{
+    this.modelService.close()
+  })
+  }
+
+  get title(){
+    return this.form.controls.title as FormControl
+  }
+
+  
+  ngOnInit(): void {
+  }
+
+}
